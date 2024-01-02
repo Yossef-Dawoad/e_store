fd038c75cc31d762e2db339f441b23494c409391
Yossef-Dawoad <yossefdawoad15@gamil.com>
2024-01-02 03:04:46 +0200

    setup firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:e_store/core/routes/app_router.dart';
 import 'package:e_store/estore_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     EStoreApp(router: AppRouter()),
   );
